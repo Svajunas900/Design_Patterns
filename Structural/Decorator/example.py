b5e554ae546828278e44a33f03ffb72174f0297d
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-05 16:55:10 +0200

    Add decorator pattern

---
+++ Structural/Decorator/example.py
@@ -0,0 +1,12 @@
+def make_blink(function):
+    def inner():
+        res = function()
+        result = "<blink>" + res + "</blink>"
+        return result
+    return inner
+
+@make_blink
+def hello_world():
+    return "Hello world"
+
+print(hello_world())
